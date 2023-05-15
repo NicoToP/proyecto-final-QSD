7f57588c62a19da7a7214cfb08180b9272741a11
Sebastian-Leal-S <sedastianleal@gmail.com>
2023-05-15 17:11:42 -0500

    punto de control #17

--- client/src/api/pets.js
+++ client/src/api/pets.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-export const getPetsRequests = async () => await axios.get('/pets');
+export const getPetsRequests = async () => await axios.get('/v1/api/pets');
 
-export const getPetRequests = async id => await axios.get(`/pets/${id}`);
+export const getPetRequests = async id => await axios.get(`/v1/api/pets/${id}`);
 
-export const createPetRequest = async pet => await axios.post('/pets', pet);
+export const createPetRequest = async pet => await axios.post('/v1/api/pets', pet);
 
-export const updatePetRequest = async (id, pet) => await axios.put(`/pets/${id}`, pet);
+export const updatePetRequest = async (id, pet) => await axios.put(`/v1/api/pets/${id}`, pet);
 
-export const deletePetRequest = async id => await axios.delete(`/pets/${id}`);
+export const deletePetRequest = async id => await axios.delete(`/v1/api/pets/${id}`);

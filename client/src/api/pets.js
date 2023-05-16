edf9ccee541766aefc28d5693b8d3cc9d3122afa
Sebastian-Leal-S <sedastianleal@gmail.com>
2023-05-15 23:34:05 -0500

    deployment

--- client/src/api/pets.js
+++ client/src/api/pets.js
@@ -1,11 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const getPetsRequests = async () => await axios.get('/pets');
+export const getPetsRequests = async () => await axios.get("/api/v1/pets");
 
-export const getPetRequests = async id => await axios.get(`/pets/${id}`);
+export const getPetRequests = async (id) =>
+  await axios.get(`/api/v1/pets/${id}`);
 
-export const createPetRequest = async pet => await axios.post('/pets', pet);
+export const createPetRequest = async (pet) =>
+  await axios.post("/api/v1/pets", pet);
 
-export const updatePetRequest = async (id, pet) => await axios.put(`/pets/${id}`, pet);
+export const updatePetRequest = async (id, pet) =>
+  await axios.put(`/api/v1/pets/${id}`, pet);
 
-export const deletePetRequest = async id => await axios.delete(`/pets/${id}`);
+export const deletePetRequest = async (id) =>
+  await axios.delete(`/api/v1/pets/${id}`);

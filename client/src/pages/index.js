c1318556690178f70a4b75a67116d00023e41e63
NicoToP <you@example.com>
2023-05-17 10:00:15 -0500

    Se agregan archivos de imagenes, componentes como footer y navbar, paginas de producto y servicios

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,3 +1,5 @@
 export {HomePage} from './HomePage';
 export {NotFoundPage} from './NotFoundPage';
 export {PetPage} from './PetPage';
+export {ServicesPage} from './ServicesPage';
+export {ProductsPage} from './ProductsPage';

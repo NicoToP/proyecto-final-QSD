ac85e32ab0d49036b2bc2f8770c9a8da8536fd5d
Sebastian-Leal-S <sedastianleal@gmail.com>
2023-05-17 11:58:19 -0500

    punto de control

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,38 +1,15 @@
 import { Link } from "react-router-dom";
 
-export function Navbar() {
-  const menuData = [
-    {
-      path: "/",
-      name: "Home",
-    },
-    {
-      path: "/appointments",
-      name: "Citas Medicas",
-    },
-    {
-      path: "/services",
-      name: "Servicios",
-    },
-    {
-      path: "/products",
-      name: "Productos",
-    },
-    {
-      path: "/contact",
-      name: "Contactanos",
-    },
-  ];
-
+export function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary bg-gradient mb-5">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary bg-gradient ">
       <div className="container-fluid p-2">
         <Link className="navbar-brand fs-2 mx-4" to="/">
           <img
             src="/img/logo.png"
             alt="Logo"
-            width="50"
-            height="50"
+            width="70"
+            height="70"
             className="d-inline-block align-text-center mx-2"
           />
           Vet-Vet
@@ -41,28 +18,47 @@ export function Navbar() {
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
+          data-bs-target="#navbarColor01"
+          aria-controls="navbarColor01"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            {menuData.map((item) => (
-              <li key={item.name} className="nav-item fs-5 mx-3">
-                <Link to={item.path} className="nav-link">
-                  <div className="list-item">{item.name}</div>
-                </Link>
-              </li>
-            ))}
+
+        <div className="collapse navbar-collapse justify-content-center">
+          <ul className="navbar-nav">
+            <li className="nav-item fs-5 mx-3">
+              <Link className="nav-link" to="/">
+                Inicio
+              </Link>
+            </li>
+            <li className="nav-item fs-5  mx-3">
+              <Link className="nav-link" to="/appointments">
+                Citas Medicas
+              </Link>
+            </li>
+            <li className="nav-item fs-5  mx-3">
+              <Link className="nav-link" to="/services">
+                Servicios
+              </Link>
+            </li>
+            <li className="nav-item fs-5  mx-3">
+              <Link className="nav-link" to="/products">
+                Productos
+              </Link>
+            </li>
+            <li className="nav-item fs-5  mx-3">
+              <Link className="nav-link" to="/contact">
+                Contactanos
+              </Link>
+            </li>
+            <li className="nav-item fs-5  mx-3">
+              <Link classNameName="nav-link" to="/profile">
+                Perfil
+              </Link>
+            </li>
           </ul>
-          <div className="ms-auto me-auto">
-            <Link to="/profile" className="btn btn-success">
-              Registrate
-            </Link>
-          </div>
         </div>
       </div>
     </nav>

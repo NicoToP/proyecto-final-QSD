fcdb11da64103a8245096fa3cb203b64605952c4
Sebastian-Leal-S <sedastianleal@gmail.com>
2023-05-17 10:20:57 -0500

    punto de control

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,62 +1,60 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export function NavBar() {
   return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary bg-gradient ">
+      <div className="container-fluid p-2">
+        <Link className="navbar-brand fs-2 mx-4" to="/">
+          <img
+            src="/img/logo.png"
+            alt="Logo"
+            width="70"
+            height="70"
+            className="d-inline-block align-text-center mx-2"
+          />
+          Vet-Vet
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarColor01"
+          aria-controls="navbarColor01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary bg-gradient ">
-      <div class="container-fluid p-2">
-          <Link class="navbar-brand fs-2 mx-4" href="/">
-            <img
-              src="/img/logo.png"
-              alt="Logo"
-              width="70"
-              height="70"
-              class="d-inline-block align-text-center mx-2"
-            />
-            Vet-Vet
-          </Link>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-        <div class="collapse navbar-collapse justify-content-center">
-          <ul class="navbar-nav">
-            <li class="nav-item fs-5 mx-3">
-              <Link class="nav-link {{#if home}}active{{/if}}" href="/">
+        <div className="collapse navbar-collapse justify-content-center">
+          <ul className="navbar-nav">
+            <li className="nav-item fs-5 mx-3">
+              <Link className="nav-link" to="/">
                 Inicio
               </Link>
             </li>
-            <li class="nav-item fs-5  mx-3">
-              <Link
-                class="nav-link {{#if appointments}}active{{/if}}"
-                href="/appointments"
-              >
+            <li className="nav-item fs-5  mx-3">
+              <Link className="nav-link" to="/appointments">
                 Citas Medicas
               </Link>
             </li>
-            <li class="nav-item fs-5  mx-3">
-              <Link
-                class="nav-link {{#if services}}active{{/if}}"
-                href="/services"
-              >
+            <li className="nav-item fs-5  mx-3">
+              <Link className="nav-link" to="/services">
                 Servicios
               </Link>
             </li>
-            <li class="nav-item fs-5  mx-3">
-              <Link
-                class="nav-link {{#if products}}active{{/if}}"
-                href="/products"
-              >
+            <li className="nav-item fs-5  mx-3">
+              <Link className="nav-link" to="/products">
                 Productos
               </Link>
             </li>
-            <li class="nav-item fs-5  mx-3">
-              <Link class="nav-link {{#if contact}}active{{/if}}" href="/contact">
+            <li className="nav-item fs-5  mx-3">
+              <Link className="nav-link" to="/contact">
                 Contactanos
               </Link>
             </li>
-            <li class="nav-item fs-5  mx-3">
-              <Link class="nav-link {{#if profile}}active{{/if}}" href="/profile">
+            <li className="nav-item fs-5  mx-3">
+              <Link classNameName="nav-link" to="/profile">
                 Perfil
               </Link>
             </li>
